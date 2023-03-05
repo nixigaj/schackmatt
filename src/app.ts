ded465f0f930bbb0907ff3fa9aae755590c78274
Martin Ek <martin.ek1@icloud.com>
2023-03-05 22:31:51 +0100

    Test push

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,23 @@ import http from 'http';
 
 const app: Express = express();
 app.use(express.static('public'));
-const port: number | string = process.env.PORT || 3000;
+let port: number | string = process.env.PORT || 3000;
+const args: string[] = process.argv.slice(2);
+let path: string = process.argv[2] || 'dist/index.html';
+
+if (args[0] === '--port') {
+    port = Number(args[1]);
+}
+
+if (args[2] === '--path') {
+    path = args[3];
+}
 
 app.get('/', (req: Request, res: Response) => {
-    res.sendFile(__dirname + '/dist/index.html');
+    res.sendFile(__dirname + '/' + path);
 });
 
 http.createServer(app).listen(port, () => {
-//app.listen(port, () => {
     console.log(`listening on port: ${port}`);
 });
 
